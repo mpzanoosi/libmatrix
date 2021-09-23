7fa7327d712b3ef7561a7adb91c5582d9d151105
mohamadreza <you@example.com>
2021-09-23 06:57:27 +0330

    reverting matrix_func_exec: this function is the one which execute an element-wise operation on a matrix
    matrix_funct_exec_label will be a function related to scenario 2 of previous commit

--- src/funcs.c
+++ src/funcs.c
@@ -4,21 +4,14 @@ struct matrix *matrix_func_exec(struct matrix *m, double (*f)(double))
 {
     struct matrix *result = matrix_dup(m);
     int i;
-    if (m->l_count > 0) {
-        for (i = 0; i < result->e_count; i++) {
-            result->e[i] = f(m->l[i]);
-        }
-    } else {
-        for (i = 0; i < result->e_count; i++) {
-            result->e[i] = f(m->e[i]);
-        }
+    for (i = 0; i < result->e_count; i++) {
+        result->e[i] = f(m->e[i]);
     }
     return result;
 }
 
 struct matrix *matrix_func_exec_label(struct matrix *m, double (*f)(double))
 {
-    printf("----just 1D-----\n");
     struct matrix *result = matrix_dup(m);
     matrix_print_metadata(result);
     size_t i, offset;
