ce49613bb7fb88da9f5859392b9f983a3d9ecd30
mohamadreza <m.zanoosi@gmail.com>
2022-06-30 00:25:14 -0400

    dummy commit after a long break (after dota :))

--- src/init/matrix.h
+++ src/init/matrix.h
@@ -16,22 +16,47 @@ struct matrix {
     size_t *dims; //<! number of elements at each dimension
     size_t e_count; //<! total # elements = multiplication of values in 'dims'
     double *e; //<! all elements in a vectorized array
-    double **e2; //<! a 2D mapping of 'e' just for 2D matrices
-    size_t l_count; //<! total # labels = sum of values in 'dims'
-    double *l; //<! inserted tags or labels for each dimension
 };
 
 typedef struct matrix *(*matrix_func)(struct matrix *);
 
 // ***** core methods ***** //
 
+struct matrix *matrix_init(size_t dim_count, size_t *dims);
+
+struct matrix *matrix_dup(struct matrix *m);
+
+int matrix_destroy(struct matrix *m);
+
+int matrix_destroy_batch(int count, ...);
+
+// get methods
+
+struct matrix *matrix_get_part(struct matrix *m, struct matrix **indices);
+
+// 2d matrix manipulations
+
+struct matrix *matrix_get_row(struct matrix *m, int row);
+
+struct matrix *matrix_get_col(struct matrix *m, int col);
+
+struct matrix *matrix_set_row(struct matrix *m, struct matrix *row);
+
+struct matrix *matrix_set_col(struct matrix *m, struct matrix *col);
+
+
+
+
+
+
+
 struct matrix *matrix_init_empty(size_t dim_count, size_t *dims);
 
-struct matrix *matrix_init_empty_labels(size_t dim_count, struct matrix **labels);
+// struct matrix *matrix_init_empty_labels(size_t dim_count, struct matrix **labels);
 
 struct matrix *matrix_init(size_t dim_count, size_t *dims);
 
-struct matrix *matrix_init_labels(size_t dim_count, struct matrix **labels);
+// struct matrix *matrix_init_labels(size_t dim_count, struct matrix **labels);
 
 int matrix_destroy(struct matrix *m);
 
@@ -45,11 +70,14 @@ struct matrix *matrix_dup(struct matrix *m);
 
 int matrix_set_element(struct matrix *m, size_t *pos, double e);
 
-size_t matrix_label_offset(struct matrix *m, size_t dim);
+// size_t matrix_label_offset(struct matrix *m, size_t dim);
 
-int matrix_set_labels(struct matrix *m, size_t dim, double *l);
+// int matrix_set_labels(struct matrix *m, size_t dim, double *l);
 
-int matrix_set_label_by_range(struct matrix *m, size_t dim, struct matrix *range);
+// int matrix_set_label_by_range(struct matrix *m, size_t dim, struct matrix *range);
+
+
+
 
 struct matrix *matrix_range(double x1, double x2, double dx);
 
